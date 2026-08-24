4cd9bb004f77be792b5d0def3cfc4e07239fa8f4
leunghn-tech <2.64291194e+08+leunghn-tech@users.noreply.github.com>
2026-08-24 05:57:34 +0000

    Add P1 math difficulty indicators

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './welitQuest.css';
+import './p1Difficulty.css';
 
 createRoot(document.getElementById('root')).render(<App />);
